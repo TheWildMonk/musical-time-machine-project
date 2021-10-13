f687b11f0c953099b2c3831b5a1368a52ccc02ac
Shuvo Nasir <shuvonasir028@gmail.com>
2021-10-13 06:06:10 +0600

    finalize the project

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ load_dotenv("/Volumes/Workstation/Learning Center/Data Science/"
             "100 Days of Code - Complete Python Pro Bootcamp 2021/Projects/@CREDENTIALS/.env")
 SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
 SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
-REDIRECT_URI = "http://example.com"
+REDIRECT_URI = "https://example.com"
 
 # Spotipy set up for Spotify authorization
 auth_manager = SpotifyOAuth(client_id=SPOTIFY_CLIENT_ID, client_secret=SPOTIFY_CLIENT_SECRET,
@@ -23,7 +23,11 @@ user_id = spotify_auth.me()["id"]
 # User input for date
 user_input_date = input("Which year do you want to travel to? Type the date [Date Format: YYYY-MM-DD]}: ")
 date = dt.strptime(user_input_date, "%Y-%m-%d").date()
-# playlist = spotify_auth.user_playlist_create(user=user_id, name=f"{date} Billboard 100")
+
+# Create playlist
+playlist = spotify_auth.user_playlist_create(user=user_id, name=f"{date} Billboard 100",
+                                             public=False)
+playlist_id = playlist["id"]
 
 # Billboard site's HTML
 response = requests.get(url=f"https://www.billboard.com/charts/hot-100/{date}")
@@ -36,11 +40,14 @@ raw_song_titles = soup.find_all(name="span", class_="chart-element__information_
 song_titles = [titles.getText() for titles in raw_song_titles]
 
 # Extracting Spotify URI
+tracks = []
 for track in song_titles:
     try:
         result = spotify_auth.search(q=f"name: {track} "
-                                       f"year: {date.year}")["tracks"]["items"][0]["album"]["artists"][0]["uri"]
+                                       f"year: {date.year}")["tracks"]["items"][0]["uri"]
+        tracks.append(result)
     except IndexError:
         pass
-    else:
-        print(result)
+
+# Add tracks to the playlist
+spotify_auth.playlist_add_items(playlist_id=playlist_id, items=tracks, position=None)
